54eb2559547b29a4ff38519049ae51da1a440b38
Alex Clark <aclark@aclark.net>
2011-08-13 23:56:21 -0400

    Add version check example

--- bbb/sucks/__init__.py
+++ bbb/sucks/__init__.py
@@ -13,9 +13,18 @@ try:
 except ImportError:
     from Products.Five import zcml
 
+# http://plone.org/documentation/manual/upgrade-guide/version/upgrading-plone-3-x-to-4.0/updating-add-on-products-for-plone-4.0/detecting-plone-4
+try: 
+    # Plone 4 and higher 
+    import plone.app.upgrade 
+    PLONE_VERSION = 4
+except ImportError: 
+    PLONE_VERSION = 3
+
 def main():
     print 'BBB sucks!'
     print '=========='
+    print 'Version: Plone %d' % PLONE_VERSION
     print 'Imported: %s' % cp
     print 'Imported: %s' % zcml
 
