6b118e7ad00ebac2c0b33fe5c2bdc0949e5a71f2
Alex Clark <aclark@aclark.net>
2011-08-13 23:49:06 -0400

    Add zcml example

--- bbb/sucks/__init__.py
+++ bbb/sucks/__init__.py
@@ -7,8 +7,15 @@ try:
 except SyntaxError:
     import ConfigParser as cp
 
+# BBB Zope 2.12
+try:
+    from Zope2.App import zcml
+except ImportError:
+    from Products.Five import zcml
+
 def main():
     print 'BBB sucks!'
     print '=========='
     print 'Imported: %s' % cp
+    print 'Imported: %s' % zcml
 
