9c2671fc74307ba7fe100f8c2b1ed5f8a809c404
Alex Clark <aclark@aclark.net>
2011-08-13 23:34:03 -0400

    Roll with Plone 4

--- setup.py
+++ setup.py
@@ -4,14 +4,12 @@ from setuptools import setup, find_packages
 install_requires=[
     'setuptools',
     'collective.transmogrifier',
-],
+]
 
 try:
-    # If we are using Python 2.5 or greater we can require configparser
     eval("1 if True else 2")  # http://stackoverflow.com/questions/446052
     install_requires.append('configparser')
 except SyntaxError:
-    # If we are using Python 2.4 or lower we cannot require configparser
     pass
 
 setup(
