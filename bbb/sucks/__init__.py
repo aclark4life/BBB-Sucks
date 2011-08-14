a822fc4058586e735058a33d97b6b6a012eef89d
Alex Clark <aclark@aclark.net>
2011-08-13 23:28:15 -0400

    Make it fancy

--- bbb/sucks/__init__.py
+++ bbb/sucks/__init__.py
@@ -3,9 +3,12 @@ import collective.transmogrifier
 
 try:
     eval("1 if True else 2")  # http://stackoverflow.com/questions/446052
-    import configparser
+    import configparser as cp
 except SyntaxError:
-    import ConfigParser
+    import ConfigParser as cp
 
 def main():
     print 'BBB sucks!'
+    print '=========='
+    print 'Imported: %s' % cp
+
