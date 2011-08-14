85c63c149c88b25170b286f5dfa58ef9f7219657
Alex Clark <aclark@aclark.net>
2011-08-13 23:26:14 -0400

    Add cp example

--- bbb/sucks/__init__.py
+++ bbb/sucks/__init__.py
@@ -1,5 +1,11 @@
 
 import collective.transmogrifier
 
+try:
+    eval("1 if True else 2")  # http://stackoverflow.com/questions/446052
+    import configparser
+except SyntaxError:
+    import ConfigParser
+
 def main():
-    pass
+    print 'BBB sucks!'
--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 
 from setuptools import setup, find_packages
 
+install_requires=[
+    'setuptools',
+    'collective.transmogrifier',
+],
+
+try:
+    # If we are using Python 2.5 or greater we can require configparser
+    eval("1 if True else 2")  # http://stackoverflow.com/questions/446052
+    install_requires.append('configparser')
+except SyntaxError:
+    # If we are using Python 2.4 or lower we cannot require configparser
+    pass
+
 setup(
     name='bbb.sucks',
     packages=find_packages(),
     namespace_packages=[
         'bbb',
     ],
-    install_requires=[
-        'setuptools',
-        'collective.transmogrifier',
-    ],
+    install_requires=install_requires,
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone',
         'console_scripts': 'bbb = bbb.sucks:main',
