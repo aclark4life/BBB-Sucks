708ae163ddef3d9e9e92f4cdc2b7bb5925e1730c
Alex Clark <aclark@aclark.net>
2011-08-14 00:03:18 -0400

    Add zope.component example

--- bbb/sucks/__init__.py
+++ bbb/sucks/__init__.py
@@ -1,5 +1,6 @@
 
 import collective.transmogrifier
+import zope.component as zc
 
 try:
     eval("1 if True else 2")  # http://stackoverflow.com/questions/446052
@@ -7,16 +8,13 @@ try:
 except SyntaxError:
     import ConfigParser as cp
 
-# BBB Zope 2.12
 try:
     from Zope2.App import zcml
 except ImportError:
     from Products.Five import zcml
 
-# http://plone.org/documentation/manual/upgrade-guide/version/upgrading-plone-3-x-to-4.0/updating-add-on-products-for-plone-4.0/detecting-plone-4
-try: 
-    # Plone 4 and higher 
-    import plone.app.upgrade 
+try:  
+    import plone.app.upgrade  # http://plone.org/documentation/manual/upgrade-guide/version/upgrading-plone-3-x-to-4.0/updating-add-on-products-for-plone-4.0/detecting-plone-4
     PLONE_VERSION = 4
 except ImportError: 
     PLONE_VERSION = 3
@@ -26,5 +24,6 @@ def main():
     print '=========='
     print 'Version: Plone %d' % PLONE_VERSION
     print 'Imported: %s' % cp
+    print 'Imported: %s' % zc
     print 'Imported: %s' % zcml
 
